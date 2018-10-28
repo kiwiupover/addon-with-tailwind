4730f225a00cb0281a573e6e9676d1d24078effb
Dave Laird <kiwiupover@gmail.com>
2018-10-28 10:14:10 -0700

    adding tailwind

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
+const MergeTrees = require('broccoli-merge-trees');
+const buildTailwind = require('ember-cli-tailwind/lib/build-tailwind');
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  config() {
+    return {
+      'ember-cli-tailwind': {
+        shouldBuildTailwind: false
+      }
+    };
+  },
+
+  treeForAddonStyles(tree) {
+    let trees = tree ? [ tree ] : [];
+
+    trees.push(buildTailwind(this));
+
+    return new MergeTrees(trees);
+  }
 };
